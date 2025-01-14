54601885e5b0ed19d51833050f3ce42089a62a95
topazape <topazape@gmail.com>
2025-01-14 16:46:27 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# OldTale.nvim
---
+++ lua/oldworld/groups/integrations/noice.lua
@@ -0,0 +1,8 @@
+local p = require("oldworld.palette")
+
+return {
+    NoiceCmdlinePopup = { bg = p.bg_dark },
+    NoiceCmdlinePopupBorder = { fg = p.blue, italic = true },
+    NoiceCmdlinePopupTitle = { fg = p.red, italic = true },
+    NoiceCmdlineIcon = { fg = p.blue },
+}
