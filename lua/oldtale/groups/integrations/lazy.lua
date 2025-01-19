77bfe3e773610afc828712dad4366d0b5470d6cd
topazape <topazape@gmail.com>
2025-01-19 12:44:55 +0900

    lazy, mason

--- lua/oldtale/groups/integrations/lazy.lua
+++ lua/oldtale/groups/integrations/lazy.lua
@@ -1,5 +1,47 @@
 local p = require("oldtale.palette")
 
 return {
-	LazyDimmed = { fg = p.subtext4 },
+	LazyBold = { bold = true },
+	LazyItalic = { italic = true },
+	LazyNormal = { fg = p.fg, bg = p.bg_dark },
+
+	LazyButton = { bg = p.gray2 },
+	LazyButtonActive = { bg = p.gray3 },
+	LazyH1 = { fg = p.bg_dark, bg = p.blue, bold = true },
+	LazyH2 = { fg = p.blue, bold = true },
+
+	LazyError = { fg = p.red },
+	LazyWarning = { fg = p.yellow },
+	LazyInfo = { fg = p.blue },
+
+	LazyCommit = { fg = p.purple },
+	LazyCommitType = { fg = p.green, bold = true },
+	LazyCommitScope = { fg = p.blue, italic = true },
+	LazyCommitIssue = { fg = p.magenta },
+
+	LazyComment = { fg = p.gray6 },
+	LazyDimmed = { fg = p.subtext2 },
+	LazyProp = { fg = p.subtext2 },
+	LazyValue = { fg = p.green },
+	LazySpecial = { fg = p.purple },
+	LazyDir = { fg = p.cyan },
+	LazyUrl = { fg = p.cyan },
+	LazyLocal = { fg = p.yellow },
+
+	LazyProgressDone = { fg = p.green },
+	LazyProgressTodo = { fg = p.gray4 },
+
+	LazyReasonCmd = { fg = p.orange },
+	LazyReasonEvent = { fg = p.yellow },
+	LazyReasonFt = { fg = p.green },
+	LazyReasonImport = { fg = p.blue },
+	LazyReasonKeys = { fg = p.magenta },
+	LazyReasonPlugin = { fg = p.purple },
+	LazyReasonRequire = { fg = p.cyan },
+	LazyReasonRuntime = { fg = p.bright_magenta },
+	LazyReasonSource = { fg = p.green },
+	LazyReasonStart = { fg = p.blue },
+
+	LazyTaskOutput = { fg = p.fg, bg = p.bg_dark },
+	LazyNoCond = { fg = p.yellow },
 }
--- lua/oldtale/groups/integrations/mason.lua
+++ lua/oldtale/groups/integrations/mason.lua
@@ -1,22 +1,26 @@
 local p = require("oldtale.palette")
 
 return {
-	MasonHeader = { fg = p.fg, bg = p.bg, bold = true },
-	MasonHeaderSecondary = { fg = p.fg, bg = p.bg, bold = true },
-	MasonHeading = { fg = p.blue, bold = true },
+	MasonNormal = { fg = p.fg, bg = p.bg_dark },
+	MasonHeader = { fg = p.bg_dark, bg = p.blue, bold = true },
+	MasonHeaderSecondary = { fg = p.bg_dark, bg = p.cyan, bold = true },
 
-	MasonHighlight = { fg = p.green },
-	MasonHighlightSecondary = { fg = p.blue },
-	MasonHighlightBlock = { fg = p.bg, bg = p.green, bold = true },
-	MasonHighlightBlockBold = { fg = p.bg, bg = p.blue, bold = true },
-	MasonHighlightBlockSecondary = { fg = p.bg, bg = p.blue },
-	MasonHighlightBlockBoldSecondary = { fg = p.bg, bg = p.blue, bold = true },
+	MasonHighlight = { fg = p.cyan },
+	MasonHighlightBlock = { fg = p.bg_dark, bg = p.cyan },
+	MasonHighlightBlockBold = { fg = p.bg_dark, bg = p.cyan, bold = true },
 
-	MasonLink = { fg = p.cyan },
-	MasonMuted = { fg = p.subtext1 },
-	MasonMutedBlock = { fg = p.subtext3, bg = p.bg, bold = true },
-	MasonMutedBlockBold = { fg = p.bg, bg = p.blue, bold = true },
+	MasonHighlightSecondary = { fg = p.yellow },
+	MasonHighlightBlockSecondary = { fg = p.bg_dark, bg = p.yellow },
+	MasonHighlightBlockBoldSecondary = { fg = p.bg_dark, bg = p.yellow, bold = true },
+
+	MasonLink = { fg = p.cyan, underline = true },
+
+	MasonMuted = { fg = p.subtext3 },
+	MasonMutedBlock = { fg = p.bg_dark, bg = p.subtext3 },
+	MasonMutedBlockBold = { fg = p.bg_dark, bg = p.subtext3, bold = true },
 
 	MasonError = { fg = p.red },
 	MasonWarning = { fg = p.yellow },
+
+	MasonHeading = { fg = p.blue, bold = true },
 }
